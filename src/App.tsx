1321f08be64bdae516336b518315438be278012e
SamuelToledo123 <Samuel.Toledo@yh.nackademin.se>
2025-05-06 16:02:09 +0200

    LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes, Link } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import Navbar from './components/Navbar';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div>
      <Navbar />
       <Routes>
+      <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<div>Welcome to Minimums!</div>} />
       </Routes>
